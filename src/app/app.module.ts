e1844e0f5aac53b72f297ec90e28646bed542f53
Gustavo <gustavomedeiros15@outlook.com.br>
2023-04-24 19:52:38 -0300

    Interceptor not finishing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { NotificationModule } from './notification/notification.module';
 import { IndexModule } from './index/index.module';
+// import { InterceptorModule } from './interceptor/interceptor.module';
 
 @Module({
-  imports: [UserModule, NotificationModule, IndexModule],
+  imports: [UserModule, NotificationModule, IndexModule, InterceptorModule],
 })
 export class AppModule {}
---
+++ src/app/interceptor/interceptor.module.ts
@@ -0,0 +1,11 @@
+// import { Module } from "@nestjs/common";
+// import { InterceptorController } from "./interceptor.controller";
+// import { InterceptorService } from "./interceptor.service";
+
+// @Module({
+//     imports: [],
+//     controllers: [InterceptorController],
+//     providers: [InterceptorService]
+// })
+
+// export class InterceptorModule { }
